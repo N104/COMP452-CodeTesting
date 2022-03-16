8e8a759d9f3b879ffb7bf1122de0a8b7d68bdf0d
kespaan <57606567+kespaan@users.noreply.github.com>
2022-03-15 23:13:12 -0400

    Added tests to Stats Panel Test

--- test/StatsPanelTest.java
+++ test/StatsPanelTest.java
@@ -20,10 +20,21 @@ public class StatsPanelTest {
     void CalculateUnorderedBinCountsTest() {
         GameStatsStub stats = new GameStatsStub(Map.of(
                 7, 4,
-                3, 2
+                3, 2,
+                5, 3
         ));
         int[] counts = StatsPanel.calculateBinCounts(stats);
         assertEquals(4, counts[3]);
-        assertEquals(3, counts[1]);
+        assertEquals(3,counts[2]);
+        assertEquals(2, counts[1]);
+    }
+    @Test
+    void CalculateHighValues() {
+        GameStatsStub stats = new GameStatsStub(Map.of(
+                16, 2,
+                18, 4
+        ));
+        int[] counts = StatsPanel.calculateBinCounts(stats);
+        assertEquals(6, counts[7]);
     }
 }
