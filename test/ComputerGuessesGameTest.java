44f92a4e9a0fea2796f707710bb4203242a63585
kespaan <57606567+kespaan@users.noreply.github.com>
2022-03-17 18:23:30 -0400

    More ComputerGuessesGame Tests

--- test/ComputerGuessesGameTest.java
+++ test/ComputerGuessesGameTest.java
@@ -46,6 +46,28 @@ public class ComputerGuessesGameTest {
         game.guessLower();
         assertEquals(1, game.getLastGuess());
     }
+    @Test
+    void GuessHigherTest() {
+        // the number we picked for the computer to guess is 1000
+        ComputerGuessesGame game = new ComputerGuessesGame(1, 1000);
+        assertEquals(500, game.getLastGuess());
+        game.guessHigher();
+        assertEquals(751, game.getLastGuess());
+        game.guessHigher();
+        assertEquals(876, game.getLastGuess());
+        game.guessHigher();
+        assertEquals(939, game.getLastGuess());
+        game.guessHigher();
+        assertEquals(970, game.getLastGuess());
+        game.guessHigher();
+        assertEquals(986, game.getLastGuess());
+        game.guessHigher();
+        assertEquals(994, game.getLastGuess());
+        game.guessHigher();
+        assertEquals(998, game.getLastGuess());
+        game.guessHigher();
+        assertEquals(1000, game.getLastGuess());
+    }
 
     @Test void ResetGameTest() {
         ComputerGuessesGame game = new ComputerGuessesGame(1, 500);
